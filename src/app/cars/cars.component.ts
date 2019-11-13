3132803de2f4a36ad0e5f2f8c743583f41f5e358
Rozenberg-jv <avangard.npaper@gmail.com>
2019-11-13 20:15:34 +0300

    section 2 finished

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -17,6 +17,14 @@ export class CarsComponent {
     {id: 3, name: 'item 3'},
     {id: 4, name: 'item 4'}
   ];
+  dates = [
+    new Date(2015, 3, 2),
+    new Date(2013, 1, 11),
+    new Date(1952, 12, 30),
+    new Date(2012, 2, 5),
+    new Date(1995, 23, 15),
+    new Date(2019, 3, 11),
+  ];
 
   constructor() {
     setTimeout(() => {
